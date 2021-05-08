c7c382010c7992e68ee25b9a3073753151483759
Nyankoye Yves LOUA <ny.loua.21@eigsi.fr>
2021-05-08 09:50:03 +0200

    corrections

--- soft_desk/api/permissions.py
+++ soft_desk/api/permissions.py
@@ -1,13 +1,21 @@
 from rest_framework.permissions import BasePermission
 from rest_framework import permissions
 from .models import Contributor, Project
+
+
 class IsAuthorOrReadOnly(BasePermission):
+    """
+        Il faut être l'auteur d'un projet pour le supprimé ou modifié 
+    """
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author_user_id == request.user
 
 class IsProjectAuthorOrReadOnly(BasePermission):
+    """
+        Pour ajouter des collaorateurs à un projet il faut être l'auteur
+    """
     def has_permission(self, request, view):
         id = request.resolver_match.kwargs.get('project_id')
         project = Project.objects.get(id=id)
@@ -18,6 +26,10 @@ class IsProjectAuthorOrReadOnly(BasePermission):
         return False
 
 class ContributorsOnly(BasePermission):
+    """
+        Seule les contributeur d'un projet pouront afficher les problemes et commentaire 
+        du projet
+    """
     def has_permission(self, request, view):
         id = request.resolver_match.kwargs.get('project_id')
         project = Project.objects.get(id=id)
